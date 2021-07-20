42d0815007e95252d62b23942d4109e7f212b2cd
sanha <kgpaper@naver.com>
2021-07-20 21:46:50 +0900

    21/07/20 studying...

---
+++ src/Wrapper.js
@@ -0,0 +1,12 @@
+import React, { Children } from "react";
+
+const Wrapper = ({children}) => {
+  const style = {
+    border: "2px solid black",
+    padding: "16px",
+  };
+
+  return <div style={style}>{children}</div>;
+};
+
+export default Wrapper;
