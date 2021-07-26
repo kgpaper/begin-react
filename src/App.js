bdd9620a1313288c79fc9a28259d0c5ff8efb13c
sanha <kgpaper@naver.com>
2021-07-26 22:01:58 +0900

    21/07/26 studying...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from "react";
-import InputSample from "./InputSample";
+import React, { useRef } from 'react';
+import InputSample from './InputSample';
+import UserList from './UserList';
+
+// 1.13 할 차례
 
 function App() {
-  return <InputSample />;
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+    },
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    nextId.current += 1;
+  };
+
+  return <UserList users={users} />;
 }
 
 export default App;
