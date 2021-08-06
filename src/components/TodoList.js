606762f0fdefc5eda1956505ddaaf4d97d7a413c
sanha <kgpaper@naver.com>
2021-08-06 18:40:48 +0900

    21/08/06 studying...

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './TodoList.module.css';
+import TodoItem from './TodoItem'
 import classNames from 'classnames/bind';
 
 const style = classNames.bind(styles)
@@ -7,7 +8,10 @@ const style = classNames.bind(styles)
 const TodoList = () => {
   return (
     <div className={style('div')}>
-      TodoLIST
+      <TodoItem text="프로젝트 생성하기" done={true} />
+      <TodoItem text="컴포넌트 스타일링 하기" done={true} />
+      <TodoItem text="Context 만들기" done={false} />
+      <TodoItem text="기능 구현하기" done={false} />
     </div>
   )
 };
