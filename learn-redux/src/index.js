c721d0e0f107164b16f15cc31b9055c131aed801
sanha <kgpaper@naver.com>
2021-08-11 22:43:19 +0900

    21/08/11 studying...

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import rootReducer from "./modules";
 
+const store = createStore(rootReducer);
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
